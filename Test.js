7402b70ab4f260e8f40e9bf02907c8fd507f5e28
Qupergo <swesebbe3336@gmail.com>
2019-08-30 20:25:57 +0200

    Created folders and moved files

--- Test.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $( "input[type=checkbox]" ).on("change", function(){
-        if($(this).is(':checked'))
-            $(this).parent().css('background-color', '#4CAF50');
-        else
-            $(this).parent().css('background-color', '');
-    });
-});
