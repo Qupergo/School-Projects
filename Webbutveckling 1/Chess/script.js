c4d6fa1bd5c4824d4957b4219438944c84bf96fc
Qupergo <45945560+Qupergo@users.noreply.github.com>
2019-08-31 15:45:24 +0200

    Made movement somewhat possible
    You can now move the king, bishop, knight and rook, however, they ignore other pieces also made some tweaks to how the pieces positions are calculated

--- Webbutveckling 1/Chess/script.js
+++ Webbutveckling 1/Chess/script.js
@@ -1,42 +1,45 @@
+
+
+
 class chess_piece {
-  constructor(position, color, piece, starting_square) {
-    this.position = position;
+  constructor(color, piece, starting_square, position=[0,0]) {
     this.color = color;
     this.piece = piece
     this.identifier = starting_square
+    this.position = position;
   }
 }
 
 let chess_piece_url = "Purple_Blue/"
 
 //White pieces
-let white_rook_a = new chess_piece([0,0], "White", "Rook", "A");
-let white_rook_h = new chess_piece([0,7], "White", "Rook", "H");
-let white_knight_b = new chess_piece([0, 1], "White", "Knight", "B");
-let white_knight_g = new chess_piece([0, 6], "White", "Knight", "G");
-let white_bishop_c = new chess_piece([0, 2], "White", "Bishop", "C");
-let white_bishop_f = new chess_piece([0, 5], "White", "Bishop", "F");
-let white_king = new chess_piece([0, 3], "White", "King", "D");
-let white_queen = new chess_piece([0, 4], "White", "Queen", "E");
-let white_pawns = [new chess_piece([1, 0], "White", "Pawn", "A"), new chess_piece([1, 1], "White", "Pawn", "B"), new chess_piece([1, 2], "White", "Pawn", "C"), new chess_piece([1, 3], "White", "Pawn", "D"), new chess_piece([1, 4], "White", "Pawn", "E"), new chess_piece([1, 5], "White", "Pawn", "F"), new chess_piece([1, 6], "White", "Pawn", "G"), new chess_piece([1, 7], "White", "Pawn", "G")];
+let white_rook_a = new chess_piece("White", "Rook", "A");
+let white_rook_h = new chess_piece("White", "Rook", "H");
+let white_knight_b = new chess_piece("White", "Knight", "B");
+let white_knight_g = new chess_piece("White", "Knight", "G");
+let white_bishop_c = new chess_piece("White", "Bishop", "C");
+let white_bishop_f = new chess_piece("White", "Bishop", "F");
+let white_king = new chess_piece("White", "King", "D");
+let white_queen = new chess_piece("White", "Queen", "E");
+let white_pawns = [new chess_piece("White", "Pawn", "A"), new chess_piece("White", "Pawn", "B"), new chess_piece("White", "Pawn", "C"), new chess_piece("White", "Pawn", "D"), new chess_piece("White", "Pawn", "E"), new chess_piece("White", "Pawn", "F"), new chess_piece("White", "Pawn", "G"), new chess_piece("White", "Pawn", "G")];
 
 
 
 //Black pieces
-let black_rook_a = new chess_piece([7,0], "Black", "Rook", "A");
-let black_rook_h = new chess_piece([7,7], "Black", "Rook", "H");
-let black_knight_b = new chess_piece([7, 1], "Black", "Knight", "B");
-let black_knight_g = new chess_piece([7, 6], "Black", "Knight", "G");
-let black_bishop_c = new chess_piece([7, 2], "Black", "Bishop", "C");
-let black_bishop_f = new chess_piece([7, 5], "Black", "Bishop", "F");
-let black_king = new chess_piece([7, 3], "Black", "King", "D");
-let black_queen = new chess_piece([7, 4], "Black", "Queen", "E");
-let black_pawns = [new chess_piece([1, 0], "Black", "Pawn", "A"), new chess_piece([1, 1], "Black", "Pawn", "B"), new chess_piece([1, 2], "Black", "Pawn", "C"), new chess_piece([1, 3], "Black", "Pawn", "D"), new chess_piece([1, 4], "Black", "Pawn", "E"), new chess_piece([1, 5], "Black", "Pawn", "F"), new chess_piece([1, 6], "Black", "Pawn", "G"), new chess_piece([1, 7], "Black", "Pawn", "G")];
+let black_rook_a = new chess_piece("Black", "Rook", "A");
+let black_rook_h = new chess_piece("Black", "Rook", "H");
+let black_knight_b = new chess_piece("Black", "Knight", "B");
+let black_knight_g = new chess_piece("Black", "Knight", "G");
+let black_bishop_c = new chess_piece("Black", "Bishop", "C");
+let black_bishop_f = new chess_piece("Black", "Bishop", "F");
+let black_king = new chess_piece("Black", "King", "D");
+let black_queen = new chess_piece("Black", "Queen", "E");
+let black_pawns = [new chess_piece("Black", "Pawn", "A"), new chess_piece("Black", "Pawn", "B"), new chess_piece("Black", "Pawn", "C"), new chess_piece("Black", "Pawn", "D"), new chess_piece("Black", "Pawn", "E"), new chess_piece("Black", "Pawn", "F"), new chess_piece("Black", "Pawn", "G"), new chess_piece("Black", "Pawn", "G")];
 
 
 
 
-let piece_positions = [
+var piece_positions = [
 						[white_rook_a, white_knight_b, white_bishop_c, white_king, white_queen, white_bishop_f, white_knight_g, white_rook_h],
 					   	[white_pawns[0], white_pawns[1], white_pawns[2], white_pawns[3], white_pawns[4], white_pawns[5], white_pawns[6], white_pawns[7]],
 					   	["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
@@ -55,62 +58,100 @@ for (let y=0; y < piece_positions.length; y++)
 	{
 		if (piece_positions[y][x] == "Empty")
 		{
-			piece_positions[y][x] = new chess_piece([x, y], "None", "Empty", "None")
+			piece_positions[y][x] = new chess_piece("None", "Empty", "None", [y, x])
+		}
+		else {
+			piece_positions[y][x].position = [y, x]
 		}
 	}
 }
 
 function move_piece(current_pos, move_pos){
-	let piece = piece_positions[current_pos[0]][current_pos[1]]
-	let legal_moves = available_moves(chess_piece.piece, chess_piece.position, piece_positions)
-	console.log(legal_moves)
-	console.log(move_pos)
-	if (legal_moves.includes(move_pos)) {
-		console.log('Legal move')
-	}
-	else {
-		console.log('Illegal move')
+	let piece = piece_positions[current_pos[0]][current_pos[1]];
+	let legal_moves = available_moves(piece.piece, piece.position, piece_positions);
+	let legal = false;
+	move_pos[0] = parseInt(move_pos[0], 10);
+	move_pos[1] = parseInt(move_pos[1], 10);
+
+	for (var i = 0; i < legal_moves.length; i++) {
+		if (legal_moves[i].equals(move_pos)) {
+			console.log('Legal move');
+			legal = true;
+			break;
+		}
 	}
 
+	if (legal)
+	{
 	//After confirming the move is legal
-	console.log("Attempting to swap " + current_pos + " with " + move_pos);
-
-	let temp = piece_positions[current_pos[0]][current_pos[1]];
+	let temp = [...piece_positions][current_pos[0]][current_pos[1]];
 	let temp2 = piece_positions[current_pos[0]][current_pos[1]].position;
 
+	piece_positions[current_pos[0]][current_pos[1]].position = piece_positions[move_pos[0]][move_pos[1]].position;
 	piece_positions[current_pos[0]][current_pos[1]] = piece_positions[move_pos[0]][move_pos[1]];
-	piece_positions[current_pos[0]][current_pos[1]] = piece_positions[move_pos[0]][move_pos[1]].position;
-	piece_positions[move_pos[0]][move_pos[1]] = temp;
+
 	piece_positions[move_pos[0]][move_pos[1]].position = temp2;
+	piece_positions[move_pos[0]][move_pos[1]] = temp;
+
 	refresh_board()
-	console.log("Success")
+	}
+	else {
+		console.log("Illegal move!")
+	}
 
 }
 
 function clicked_piece(td){
-	console.log("Clicked");
-	console.log(td.id);
 	
+
 	if (document.getElementsByClassName('clicked first').length === 1){
 		td.classList.add("clicked");
 		td.classList.add("second");
 	}
-	else {
+	
+	else{
+		let piece = piece_positions[td.id.split(",")[0]][td.id.split(",")[1]];
+		let legal_moves = available_moves(piece.piece, piece.position, piece_positions);
+		console.log(legal_moves)
+		if(legal_moves.length > 0)
+		{
+
+		
 		td.classList.add("clicked");
 		td.classList.add("first");
+		
+		//add class to all available squares to move to
+		//Very poor code, remove or fix later
+
+
+
+			for (let i = 1; i < 9; i++) {
+			    for (let j = 1; j < 9; j++) {
+			    	for (var k = 0; k < legal_moves.length; k++) {
+				    	if (legal_moves[k].equals([i-1, j-1])) {
+				    	let current_tile = document.getElementById([i-1, j-1]);
+				    	current_tile.classList.add('available');
+			    		}
+			    	}
+				}
+			}
+		}
 	}
 
 	if (document.getElementsByClassName('clicked').length === 2){
 		let first_element = document.getElementsByClassName('clicked first')[0];
 		let second_element = document.getElementsByClassName('clicked second')[0];
 
-		console.log("Attempting to swap " + first_element.id + " with " + second_element.id);
 
-  		first_element.classList.remove("clicked");
+/*  		
+		first_element.classList.remove("clicked");
   		first_element.classList.remove("first");
 
 		second_element.classList.remove("clicked");
 		second_element.classList.remove("second");
+
+*/
+		refresh_board()
 		move_piece(first_element.id.split(","), second_element.id.split(","));
 	}
 }
@@ -121,6 +162,11 @@ function refresh_board(){
 	    for (let j = 1; j < 9; j++) {
 	    	let current_tile = document.getElementById([i-1, j-1])
 	    	current_tile.style.backgroundImage = `url('ChessPieceImages/${chess_piece_url}${piece_positions[i-1][j-1].color}_${piece_positions[i-1][j-1].piece}.png')`
+	    	if (i%2 == j%2) {
+	            current_tile.className = "white";
+	        } else {
+	            current_tile.className = "black";
+	        }
 	    }
 	}
 }
@@ -154,13 +200,18 @@ function available_moves(piece, position, board){
 	let available_moves = [];
 	
 	if (piece == "Rook") {
-		for (let x; x < 9; x++) {
-			for (let y; y < 9; y++) {
+
+		for (let x = 0; x < 8; x++) {
+			for (let y = 0; y < 8; y++) {
+
+				
 				if (position[0] == x && position[1] != y) {
+					console.log('Test')
 					available_moves.push([x, y]);
 				}
 				if (position[0] != x && position[1] == y) {
 					available_moves.push([x, y]);
+					console.log('Test')
 				}
 			}
 		}
@@ -170,17 +221,102 @@ function available_moves(piece, position, board){
 		let moves = [
 		[2, 1], [-2, 1], [2, -1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]
 		];
-		for (let i = 0; i < moves.length; i++) {
+		for (var i = 0; i < moves.length; i++) {
 			testing_pos = [...position];
+
 			testing_pos[0] += moves[i][0];
 			testing_pos[1] += moves[i][1];
 
 			if (testing_pos[0] < 8 && testing_pos[0] >= 0) {
-				if (testing_pos[1] > 8 && testing_pos[1] >= 0) {
+				if (testing_pos[1] < 8 && testing_pos[1] >= 0) {
 					available_moves.push(testing_pos);
 				}
 			}
 		}
 	}
+
+	if (piece == "Bishop") {
+		let moves = [
+		[1, 1], [-1, 1], [1, -1], [-1, -1]
+		]
+
+		for (var i = 0; i < moves.length; i++) {
+			let testing_pos = [...position];
+			while (true) {
+
+				testing_pos[0] += moves[i][0];
+				testing_pos[1] += moves[i][1];
+				console.log(position)
+				console.log(testing_pos)
+				if (testing_pos[0] < 8 && testing_pos[0] >= 0) {
+					if (testing_pos[1] < 8 && testing_pos[1] >= 0) {
+						legal_move = [...testing_pos]
+						available_moves.push(legal_move);
+					}
+					else {
+						break
+					}
+				}
+				else {
+					break
+				}
+			}
+		}
+	}
+
+	if (piece == "King") {
+		let moves = [
+		[1, 1], [-1, 1], [1, -1], [-1, -1], [1, 0], [-1, 0], [0, 1], [0, -1]
+		]
+		for (var i = 0; i < moves.length; i++) {
+			testing_pos = [...position];
+
+			testing_pos[0] += moves[i][0];
+			testing_pos[1] += moves[i][1];
+
+			if (testing_pos[0] < 8 && testing_pos[0] >= 0) {
+				if (testing_pos[1] < 8 && testing_pos[1] >= 0) {
+					available_moves.push(testing_pos);
+				}
+			}
+		}
+
+	}
+
 	return available_moves
+
 }
+
+
+
+
+
+
+
+
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
