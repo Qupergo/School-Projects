2608660a6faaa10ee42ddfcb4c77ec4e6f64c782
Qupergo <swesebbe3336@gmail.com>
2019-09-11 17:44:15 +0200

    Added more snakes
    Made it possible to play multiple games at the same time, this is to later make ai play several games at the same time to speed up generations

--- Webbutveckling 1/SnakeAI/snakeScript.js
+++ Webbutveckling 1/SnakeAI/snakeScript.js
@@ -1,117 +1,182 @@
-let canvas = document.getElementById("myCanvas");
+let canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
 let ctx = canvas.getContext("2d");
 
+class Snake {
+    constructor(canvas, ctx, parts, fruit_x, fruit_y, fruit_taken, dx, dy, snake_color, fruit_color, alive=true) {
+        this.canvas = canvas
+        this.ctx = ctx
+        this.parts = parts;
+        this.fruit_x = fruit_x;
+        this.fruit_y = fruit_y;
+        this.fruit_taken = fruit_taken;
+        this.dx = dx;
+        this.dy = dy;
+        this.snake_color = snake_color;
+        this.fruit_color = fruit_color;
+        this.alive = alive;
+    }
 
-grid_size = 8
+    draw() {
+        if (this.alive == true)
+        {
+            console.log(this.snake_color);
+            this.ctx.beginPath();
+            for (let index = this.parts.length-1; index >= 0; index--) {
+                let snakePart = this.parts[index];
+                if (index === 0) {
+                    snakePart[0] += dx;
+                    snakePart[1] += dy;
+                    this.ctx.rect(snakePart[0], snakePart[1], grid_size, grid_size);
+        
+                    if (snakePart[0] === this.fruit_x && snakePart[1] === this.fruit_y)
+                    {
+                        this.fruit_taken = true
+                        this.parts.push([-grid_size, -grid_size])
+                    }
+        
+                    for (let j = 1; j < this.parts.length; j++) {
+                        if (snakePart[0] === this.parts[j][0] && snakePart[1] === this.parts[j][1]) {
+                            this.alive = false;
+                        }
+                    }
+                    if (snakePart[0] === canvas.width) {
+                        snakePart[0] = 0;
+                    }
+                    else if(snakePart[0] === 0-grid_size) {
+                        snakePart[0] = canvas.width;
+                    }
+                    else if(snakePart[1] === canvas.height) {
+                        snakePart[1] = 0;
+                    }
+                    else if(snakePart[1] === 0-grid_size) {
+                        snakePart[1] = canvas.height;
+                    }
+                    break;
+                }
+                snakePart[0] = this.parts[index - 1][0];
+                snakePart[1] = this.parts[index - 1][1];
+                
+                this.ctx.rect(snakePart[0], snakePart[1], grid_size, grid_size);
+            }
+            this.ctx.fillStyle = this.snake_color;
+            this.ctx.fill();
+            this.ctx.closePath();
 
-canvas.width = 50*grid_size
-canvas.height = 50*grid_size
+            this.ctx.beginPath();
+            this.ctx.rect(this.fruit_x, this.fruit_y, grid_size, grid_size);
+            this.ctx.fillStyle = this.fruit_color;
+            this.ctx.fill();
+            this.ctx.closePath();
+        }
+    }
+}
+
+grid_size = 15
+size = 25
+canvas.width = size*grid_size
+canvas.height = size*grid_size
 
 let dx = grid_size;
 let dy = 0;
 
-let snake = [
+snake_amount = 8;
+
+snakes = [new Snake(canvas, ctx, [
     [0,0],
     [0,grid_size],
     [0,grid_size*2],
     [0,grid_size*3],
     [0,grid_size*4],
     [0,grid_size*5]
-]
-
-let fruit_taken = false;
-let fruit_x = getRndInteger((canvas.width-grid_size)/grid_size, 0)*grid_size;
-let fruit_y = getRndInteger(0, (canvas.height-grid_size)/grid_size)*grid_size;
+], 
+    getRndInteger((canvas.width-grid_size)/grid_size, 0)*grid_size, 
+    getRndInteger(0, (canvas.height-grid_size)/grid_size)*grid_size,
+    false,
+    grid_size,
+    0,
+    "#0095DD",
+    "crimson"
+)];
+for (let index = 0; index < snake_amount-1; index++) {
+    let current_canvas = document.createElement('canvas');
+    current_canvas.width = size*grid_size;
+    current_canvas.height = size*grid_size;
+    snakes.push(new Snake(current_canvas, current_canvas.getContext('2d'), [
+        [0,0],
+        [0,grid_size],
+        [0,grid_size*2],
+        [0,grid_size*3],
+        [0,grid_size*4],
+        [0,grid_size*5]
+    ],
+        getRndInteger((canvas.width-grid_size)/grid_size, 0)*grid_size, 
+        getRndInteger(0, (canvas.height-grid_size)/grid_size)*grid_size,
+        false,
+        grid_size,
+        0,
+        getRandomColor(),
+        getRandomColor()
+    ));
+    document.body.appendChild(current_canvas);
+}
 
 document.addEventListener("keypress", function onEvent(event) {
     if (event.key === "a") {
-        dx = -grid_size
-        dy = 0
+        if (dx !== grid_size) {
+            dx = -grid_size
+            dy = 0
+        }
     }
     else if (event.key === "d") {
-        dx = grid_size
-        dy = 0
+        if (dx !== -grid_size)
+        {
+            dx = grid_size
+            dy = 0
+        }
     }
     else if (event.key === "w") {
-        dy = -grid_size
-        dx = 0
+        if (dy !== grid_size)
+        {
+            dy = -grid_size
+            dx = 0
+        }
     }
     else if (event.key === "s") {
-        dy = grid_size
-        dx = 0
+        if (dy !== -grid_size)
+        {
+            dy = grid_size
+            dx = 0
+        }
     }
 });
 
-function drawSnake() {
-    ctx.beginPath();
-
-    for (let index = snake.length-1; index >= 0; index--) {
-        let snakePart = snake[index];
-        
-        if (index === 0) {
-            snakePart[0] += dx;
-            snakePart[1] += dy;
-            ctx.rect(snakePart[0], snakePart[1], grid_size, grid_size);
-
-            if (snakePart[0] === fruit_x && snakePart[1] === fruit_y)
-            {
-                fruit_taken = true
-                snake.push([-grid_size, -grid_size])
-            }
-
-            for (let j = 1; j < snake.length; j++) {
-                if (snakePart[0] === snake[j][0] && snakePart[1] === snake[j][1]) {
-                    document.location.reload();
-                }
-            }
-
-            if (snakePart[0] === canvas.width) {
-                snakePart[0] = 0;
-            }
-            else if(snakePart[0] === 0-grid_size) {
-                snakePart[0] = canvas.width;
-            }
-            else if(snakePart[1] === canvas.height) {
-                snakePart[1] = 0;
-            }
-            else if(snakePart[1] === 0-grid_size) {
-                snakePart[1] = canvas.height;
-            }
-            break;
-        }
-        snakePart[0] = snake[index - 1][0];
-        snakePart[1] = snake[index - 1][1];
-        
-        ctx.rect(snakePart[0], snakePart[1], grid_size, grid_size);
-    }
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function drawFruit() {
-    ctx.beginPath();
-
-    ctx.rect(fruit_x, fruit_y, grid_size, grid_size)
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSnake();
-    drawFruit();
-    console.log(fruit_taken)
-    if (fruit_taken) {
-        fruit_x = getRndInteger((canvas.width-grid_size)/grid_size, 0)*grid_size;
-        fruit_y = getRndInteger(0, (canvas.height-grid_size)/grid_size)*grid_size;
-        fruit_taken = false;
+    for (let index = 0; index < snakes.length; index++) {
+        current_snake = snakes[index];
+        current_snake.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        current_snake.draw();
+
+        if (current_snake.fruit_taken) {
+            current_snake.fruit_x = getRndInteger((canvas.width-grid_size)/grid_size, 0)*grid_size;
+            current_snake.fruit_y = getRndInteger(0, (canvas.height-grid_size)/grid_size)*grid_size;
+            current_snake.fruit_taken = false;
+        }
     }
 }
 
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 setInterval(draw, 50);
